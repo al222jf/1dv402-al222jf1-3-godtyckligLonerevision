5d425aa61263e54586559ae0f870f71646a443f0
al222jf <al222jf@student.lnu.se>
2014-09-17 15:31:44 +0200

    Working on median, it looks good

--- 1.3 godtyckligLonerevision/1.3 godtyckligLonerevision/Program.cs
+++ 1.3 godtyckligLonerevision/1.3 godtyckligLonerevision/Program.cs
@@ -9,14 +9,47 @@ namespace _1._3_godtyckligLonerevision
     {
         static void Main(string[] args)
         {
+            //Declare varible
+            int numberOfSalaries;
+            int salaryAmount;
             //User input number of saleries
-            int numberOfSalaries = ReadInt("Ange antal löner att mata in: ");
+            numberOfSalaries = ReadInt("Ange antal löner att mata in: ");
+            if (numberOfSalaries >= 2)
+            {
+                ProcessSalaries(numberOfSalaries);
+            }
+            else
+            {
+                Console.WriteLine("den är mer än två!");
+            }
             //User inputs salaries
             
         }
         private static void ProcessSalaries(int count)
         {
-            throw new NotImplementedException();
+            int[] salaryArray = new int[count];
+
+            Console.WriteLine();
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Ange lön nummer {0}: ", i + 1);
+                salaryArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Array.Sort(salaryArray);
+            int median = salaryArray.Length;
+
+            if (median % 2 == 0)
+            {
+                int firstMedian = salaryArray[(median / 2 - 1)];
+                int secondMedian = salaryArray[(median / 2)];
+                Console.WriteLine((firstMedian + secondMedian) / 2);
+            }
+            else
+            {
+                Console.WriteLine(salaryArray[median / 2]);
+            }
         }
         private static int ReadInt(string prompt) 
         {
